dd8a972dc24cffbbe295de8a530dae4a3f869118
bae0053 <bae8719@naver.com>
2021-06-07 16:14:21 +0900

    FileDownload 구현 중

--- mentoring/src/main/java/com/hustar/mentoring/login/config/WebMvcConfig.java
+++ mentoring/src/main/java/com/hustar/mentoring/login/config/WebMvcConfig.java
@@ -1,10 +1,25 @@
 package com.hustar.mentoring.login.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 
 	
+	
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+		
+		// 파일 인코딩 설정
+		commonsMultipartResolver.setDefaultEncoding("UTF-8");
+		
+		// 업로드 파일크기 제한 ( 5 * 1024 * 1024 = 5MB)
+		commonsMultipartResolver.setMaxUploadSizePerFile(5*1024*1024);
+		
+		return commonsMultipartResolver;
+	}
 }
